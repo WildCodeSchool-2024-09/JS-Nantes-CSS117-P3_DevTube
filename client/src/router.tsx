cf8a11effcc2ee08ffce53a1279e543d0fc86840
Fabrice Atlan <fabrice.atlan.56@gmail.com>
2025-01-07 16:30:12 +0100

    Header in place, done.

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Course from "./styles/Course/Course";
+import Course from "./components/Header/Header.tsx";
 
 export const router = createBrowserRouter([
   {
