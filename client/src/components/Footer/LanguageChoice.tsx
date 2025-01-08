67078eff0a0d261866f0f061360f63949774a92b
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-01-08 11:42:58 +0100

    traduction fonctionnality added on languges choice select

--- client/src/components/Footer/LanguageChoice.tsx
+++ client/src/components/Footer/LanguageChoice.tsx
@@ -1,44 +1,41 @@
 import arrowDropdownMenu from "/arrow-dropdown-menu-for-dark-theme.png";
 import "../../styles/Footer.css";
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 export default function LanguageChoice() {
   const [isMenuOpen, setMenuOpen] = useState(false);
   const handleClick = () => {
     setMenuOpen(!isMenuOpen);
   };
+  const { t, i18n } = useTranslation();
 
+  const handleLanguageChange = (newLanguage: string) => {
+    i18n.changeLanguage(newLanguage);
+  };
+
+  const traduction = t("language");
   return (
-    <section
-      className="dropdown-language-choice"
-      aria-label="Languages selector" // provide an accessible description of the component to screen readers
-    >
+    <section className="dropdown-language-choice">
       <div className="dropdown-label">
-        <p className="dropdown-label-text" />
-        <button
-          type="button"
-          className="dropdown-btn"
-          aria-haspopup="true" // to indicate that it opens a menu
-          aria-expanded={isMenuOpen} //  to indicate the menu status (open or closed).
-          onClick={handleClick}
-        >
+        <p className="dropdown-label-text">{traduction}</p>
+        <button type="button" className="dropdown-btn" onClick={handleClick}>
           <img
             src={arrowDropdownMenu}
-            alt="arrow to expand langugae menu"
+            alt="arrow"
             className="arrow-dropdown-menu-language"
           />
         </button>
       </div>
       {isMenuOpen ? (
-        <div
-          id="dropdown-language"
-          className="dropdown-content"
-          aria-labelledby="dropdown-label"
-        >
+        <div id="dropdown-language" className="dropdown-content">
           <button
             type="button"
             className="choice-language-button"
             data-language="en"
+            onClick={(e) =>
+              handleLanguageChange(e.currentTarget.dataset.language as string)
+            }
           >
             EN
           </button>
@@ -46,6 +43,9 @@ export default function LanguageChoice() {
             type="button"
             className="choice-language-button"
             data-language="fr"
+            onClick={(e) =>
+              handleLanguageChange(e.currentTarget.dataset.language as string)
+            }
           >
             FR
           </button>
@@ -53,6 +53,9 @@ export default function LanguageChoice() {
             type="button"
             className="choice-language-button choice-spanish-button"
             data-language="sp"
+            onClick={(e) =>
+              handleLanguageChange(e.currentTarget.dataset.language as string)
+            }
           >
             SP
           </button>
