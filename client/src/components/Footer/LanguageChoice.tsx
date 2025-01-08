f5fb484a92b9f0c2eb215d5b7b0cd605c18b1216
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-01-08 10:43:47 +0100

    component LanguageChoice done

--- client/src/components/Footer/LanguageChoice.tsx
+++ client/src/components/Footer/LanguageChoice.tsx
@@ -1,3 +1,41 @@
+import arrowDropdownMenu from "/arrow-dropdown-menu-for-dark-theme.png";
+
 export default function LanguageChoice() {
-  return <div>LanguageChoice</div>;
+  return (
+    <section className="dropdown-language-choice">
+      <div className="dropdown-label">
+        <p className="dropdown-label-text" />
+        <button type="button" className="dropdown-btn">
+          <img
+            src={arrowDropdownMenu}
+            alt="arrow"
+            className="arrow-dropdown-menu-language"
+          />
+        </button>
+      </div>
+      <div id="dropdown-language" className="dropdown-content">
+        <button
+          type="button"
+          className="choice-language-button"
+          data-language="en"
+        >
+          EN
+        </button>
+        <button
+          type="button"
+          className="choice-language-button"
+          data-language="fr"
+        >
+          FR
+        </button>
+        <button
+          type="button"
+          className="choice-language-button choice-spanish-button"
+          data-language="sp"
+        >
+          SP
+        </button>
+      </div>
+    </section>
+  );
 }
