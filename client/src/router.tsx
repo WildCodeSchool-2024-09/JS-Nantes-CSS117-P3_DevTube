9e26f5f1118d3fc98d1b58e14ca9565980db96e3
ibrahim <iadam606@yahoo.fr>
2025-01-08 17:18:41 +0100

    realization of testimonial page

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Course from "./styles/Course/Course";
+import Testimonials from "./components/Testimonials/Testimonials";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {
         path: "/course",
         element: <Course />,
+      },
+      {
+        path: "/testimonials",
+        element: <Testimonials />,
       },
     ],
   },
