1575e3e16dae3f8696a943abb0e42b38004fc38d
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-01-08 11:06:22 +0100

    select open close menu dropdown langague done

--- client/src/components/Footer/LanguageChoice.tsx
+++ client/src/components/Footer/LanguageChoice.tsx
@@ -1,12 +1,18 @@
 import arrowDropdownMenu from "/arrow-dropdown-menu-for-dark-theme.png";
 import "../../styles/Footer.css";
+import { useState } from "react";
 
 export default function LanguageChoice() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const handleClick = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <section className="dropdown-language-choice">
       <div className="dropdown-label">
         <p className="dropdown-label-text" />
-        <button type="button" className="dropdown-btn">
+        <button type="button" className="dropdown-btn" onClick={handleClick}>
           <img
             src={arrowDropdownMenu}
             alt="arrow"
@@ -14,29 +20,31 @@ export default function LanguageChoice() {
           />
         </button>
       </div>
-      <div id="dropdown-language" className="dropdown-content">
-        <button
-          type="button"
-          className="choice-language-button"
-          data-language="en"
-        >
-          EN
-        </button>
-        <button
-          type="button"
-          className="choice-language-button"
-          data-language="fr"
-        >
-          FR
-        </button>
-        <button
-          type="button"
-          className="choice-language-button choice-spanish-button"
-          data-language="sp"
-        >
-          SP
-        </button>
-      </div>
+      {isMenuOpen ? (
+        <div id="dropdown-language" className="dropdown-content">
+          <button
+            type="button"
+            className="choice-language-button"
+            data-language="en"
+          >
+            EN
+          </button>
+          <button
+            type="button"
+            className="choice-language-button"
+            data-language="fr"
+          >
+            FR
+          </button>
+          <button
+            type="button"
+            className="choice-language-button choice-spanish-button"
+            data-language="sp"
+          >
+            SP
+          </button>
+        </div>
+      ) : null}
     </section>
   );
 }
