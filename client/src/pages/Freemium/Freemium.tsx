3aa172d17a39b8457116099ab7064867a2f9c0b1
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-01-08 16:05:36 +0100

    html + style title and intro

--- client/src/pages/Freemium/Freemium.tsx
+++ client/src/pages/Freemium/Freemium.tsx
@@ -1,3 +1,22 @@
-export default function Freenium() {
-  return <div>Freenium</div>;
+import "../../styles/Freemium.css";
+import bigStarFreenium from "/big-star-freenium.png";
+
+export default function Freemium() {
+  return (
+    <section className="page-freemium">
+      <section className="title-freemium">
+        <img className="orange-star-icon" src={bigStarFreenium} alt="star" />
+        <h1>Freemium</h1>
+      </section>
+      <p className="intro-text-freemium">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </section>
+  );
 }
