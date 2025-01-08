3788d3bcb946ad5f39f1dbb2338154a4accb799d
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-01-08 16:57:29 +0100

    html + style articles freemium

--- client/src/pages/Freemium/Freemium.tsx
+++ client/src/pages/Freemium/Freemium.tsx
@@ -4,12 +4,12 @@ import starWhite from "/star-white-icon.png";
 
 export default function Freemium() {
   return (
-    <section className="page-freenium">
-      <section className="title-freenium">
+    <section className="page-fremium">
+      <section className="title-fremium">
         <img className="orange-star-icon" src={bigStarFreenium} alt="star" />
         <h1>Freemium</h1>
       </section>
-      <p className="intro-text-freenium">
+      <p className="intro-text-fremium">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
@@ -18,9 +18,9 @@ export default function Freemium() {
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
         mollit anim id est laborum.
       </p>
-      <section className="information-freenium">
-        <figure className="block-information-freenium">
-          <div className="card-information-freenium">
+      <section className="information-fremium">
+        <figure className="block-information-fremium">
+          <div className="card-information-fremium">
             <h2 className="card-block card-block-1">card-title</h2>
             <p className="card-block card-block-2">"bonus-1"</p>
             <p className="card-block card-block-3">"bonus-2"</p>
@@ -35,6 +35,34 @@ export default function Freemium() {
             </div>
           </div>
         </figure>
+        <section className="articles-information-fremium">
+          <article>
+            <h2>article-title-1</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </article>
+          <article>
+            <h2>"article-title-2"</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim
+            </p>
+          </article>
+          <article>
+            <h2>article-title-3</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </article>
+        </section>
       </section>
     </section>
   );
