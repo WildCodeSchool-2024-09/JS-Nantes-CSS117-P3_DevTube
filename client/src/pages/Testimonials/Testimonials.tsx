0a497320903f9cb18b90091fe3cbbd4eeb302285
ibrahim <iadam606@yahoo.fr>
2025-01-09 15:59:52 +0100

    Résolvez les conflits avec Git

--- client/src/pages/Testimonials/Testimonials.tsx
+++ client/src/pages/Testimonials/Testimonials.tsx
@@ -1,199 +1,199 @@
 import "../../styles/Testimonials/testimonials.css";
 import "/Users/utilisateur/Desktop/projet_trois/JS-Nantes-CSS117-P3_DevTube/client/src/styles/Globals.css";
 export default function Testimonials() {
-	return (
-		<div className="carde-contaient">
-			<div>
-				<div className="testimonials-section">
-					<header className="testimonials-header">
-						<h1>Testimonials</h1>
-						<button
-							type="button"
-							className="add-testimonial-btn"
-							aria-label="Add your testimonial"
-						>
-							Add my testimonial
-						</button>
-					</header>
-					<section className="testimonial-container">
-						<div className="testimonials-cards">
-							<article className="testimonial-card">
-								<div className="back-of">
-									<img
-										className="testimonial-image"
-										src="5097224.jpg"
-										alt=" of the person giving the testimonial"
-									/>
-								</div>
+  return (
+    <div className="carde-contaient">
+      <div>
+        <div className="testimonials-section">
+          <header className="testimonials-header">
+            <h1>Testimonials</h1>
+            <button
+              type="button"
+              className="add-testimonial-btn"
+              aria-label="Add your testimonial"
+            >
+              Add my testimonial
+            </button>
+          </header>
+          <section className="testimonial-container">
+            <div className="testimonials-cards">
+              <article className="testimonial-card">
+                <div className="back-of">
+                  <img
+                    className="testimonial-image"
+                    src="5097224.jpg"
+                    alt=" of the person giving the testimonial"
+                  />
+                </div>
 
-								<section className="testimonial-info">
-									<header className="testimonial-header">
-										<div className="testimonial-name">
-											<p>John Doe</p>
-										</div>
-										<div className="testimonial-formation">
-											<h2 className="formation">Formation suivie</h2>
-											<p className="level">| Niveau avancé</p>
-										</div>
-									</header>
+                <section className="testimonial-info">
+                  <header className="testimonial-header">
+                    <div className="testimonial-name">
+                      <p>John Doe</p>
+                    </div>
+                    <div className="testimonial-formation">
+                      <h2 className="formation">Formation suivie</h2>
+                      <p className="level">| Niveau avancé</p>
+                    </div>
+                  </header>
 
-									<p>
-										Lorem ipsum dolor sit amet consectetur adipisicing elit.
-										Fugit obcaecati id neque pariatur veniam atque possimus
-										rerum iusto aperiam error.
-									</p>
-								</section>
-							</article>
-						</div>
-						<div className="testimonials-cards">
-							<article className="testimonial-card">
-								<div className="back-of">
-									<img
-										className="testimonial-image"
-										src="5097224.jpg"
-										alt="prisentation of the person giving the testimonial"
-									/>
-								</div>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Fugit obcaecati id neque pariatur veniam atque possimus
+                    rerum iusto aperiam error.
+                  </p>
+                </section>
+              </article>
+            </div>
+            <div className="testimonials-cards">
+              <article className="testimonial-card">
+                <div className="back-of">
+                  <img
+                    className="testimonial-image"
+                    src="5097224.jpg"
+                    alt="prisentation of the person giving the testimonial"
+                  />
+                </div>
 
-								<section className="testimonial-info">
-									<header className="testimonial-header">
-										<div className="testimonial-name">
-											<p>John Doe</p>
-										</div>
-										<div className="testimonial-formation">
-											<h2 className="formation">Formation suivie</h2>
-											<p className="level">| Niveau avancé</p>
-										</div>
-									</header>
+                <section className="testimonial-info">
+                  <header className="testimonial-header">
+                    <div className="testimonial-name">
+                      <p>John Doe</p>
+                    </div>
+                    <div className="testimonial-formation">
+                      <h2 className="formation">Formation suivie</h2>
+                      <p className="level">| Niveau avancé</p>
+                    </div>
+                  </header>
 
-									<p>
-										Lorem ipsum dolor sit amet consectetur adipisicing elit.
-										Fugit obcaecati id neque pariatur veniam atque possimus
-										rerum iusto aperiam error.
-									</p>
-								</section>
-							</article>
-						</div>
-						<div className="testimonials-cards">
-							<article className="testimonial-card">
-								<div className="back-of">
-									<img
-										className="testimonial-image"
-										src="5097224.jpg"
-										alt="prisentation of the person giving the testimonial"
-									/>
-								</div>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Fugit obcaecati id neque pariatur veniam atque possimus
+                    rerum iusto aperiam error.
+                  </p>
+                </section>
+              </article>
+            </div>
+            <div className="testimonials-cards">
+              <article className="testimonial-card">
+                <div className="back-of">
+                  <img
+                    className="testimonial-image"
+                    src="5097224.jpg"
+                    alt="prisentation of the person giving the testimonial"
+                  />
+                </div>
 
-								<section className="testimonial-info">
-									<header className="testimonial-header">
-										<div className="testimonial-name">
-											<p className="name">John Doe</p>
-										</div>
-										<div className="testimonial-formation">
-											<h2 className="formation">Formation suivie</h2>
-											<p className="level">| Niveau avancé</p>
-										</div>
-									</header>
+                <section className="testimonial-info">
+                  <header className="testimonial-header">
+                    <div className="testimonial-name">
+                      <p className="name">John Doe</p>
+                    </div>
+                    <div className="testimonial-formation">
+                      <h2 className="formation">Formation suivie</h2>
+                      <p className="level">| Niveau avancé</p>
+                    </div>
+                  </header>
 
-									<p className="testimonial-description">
-										Lorem ipsum dolor sit amet consectetur adipisicing elit.
-										Fugit obcaecati id neque pariatur veniam atque possimus
-										rerum iusto aperiam error.
-									</p>
-								</section>
-							</article>
-						</div>
-						<div className="testimonials-cards">
-							<article className="testimonial-card">
-								<div className="back-of">
-									<img
-										className="testimonial-image"
-										src="5097224.jpg"
-										alt="praisentation of the person giving the testimonial"
-									/>
-								</div>
+                  <p className="testimonial-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Fugit obcaecati id neque pariatur veniam atque possimus
+                    rerum iusto aperiam error.
+                  </p>
+                </section>
+              </article>
+            </div>
+            <div className="testimonials-cards">
+              <article className="testimonial-card">
+                <div className="back-of">
+                  <img
+                    className="testimonial-image"
+                    src="5097224.jpg"
+                    alt="praisentation of the person giving the testimonial"
+                  />
+                </div>
 
-								<section className="testimonial-info">
-									<header className="testimonial-header">
-										<div className="testimonial-name">
-											<p className="name">John Doe</p>
-										</div>
-										<div className="testimonial-formation">
-											<h2 className="formation">Formation suivie</h2>
-											<p className="level">| Niveau avancé</p>
-										</div>
-									</header>
+                <section className="testimonial-info">
+                  <header className="testimonial-header">
+                    <div className="testimonial-name">
+                      <p className="name">John Doe</p>
+                    </div>
+                    <div className="testimonial-formation">
+                      <h2 className="formation">Formation suivie</h2>
+                      <p className="level">| Niveau avancé</p>
+                    </div>
+                  </header>
 
-									<p className="testimonial-description">
-										Lorem ipsum dolor sit amet consectetur adipisicing elit.
-										Fugit obcaecati id neque pariatur veniam atque possimus
-										rerum iusto aperiam error.
-									</p>
-								</section>
-							</article>
-						</div>
+                  <p className="testimonial-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Fugit obcaecati id neque pariatur veniam atque possimus
+                    rerum iusto aperiam error.
+                  </p>
+                </section>
+              </article>
+            </div>
 
-						<div className="testimonials-cards">
-							<article className="testimonial-card">
-								<div className="back-of">
-									<img
-										className="testimonial-image"
-										src="5097224.jpg"
-										alt="prisentation of the person giving the testimonial"
-									/>
-								</div>
+            <div className="testimonials-cards">
+              <article className="testimonial-card">
+                <div className="back-of">
+                  <img
+                    className="testimonial-image"
+                    src="5097224.jpg"
+                    alt="prisentation of the person giving the testimonial"
+                  />
+                </div>
 
-								<section className="testimonial-info">
-									<header className="testimonial-header">
-										<div className="testimonial-name">
-											<p className="name">John Doe</p>
-										</div>
-										<div className="testimonial-formation">
-											<h2 className="formation">Formation suivie</h2>
-											<p className="level">| Niveau avancé</p>
-										</div>
-									</header>
+                <section className="testimonial-info">
+                  <header className="testimonial-header">
+                    <div className="testimonial-name">
+                      <p className="name">John Doe</p>
+                    </div>
+                    <div className="testimonial-formation">
+                      <h2 className="formation">Formation suivie</h2>
+                      <p className="level">| Niveau avancé</p>
+                    </div>
+                  </header>
 
-									<p className="testimonial-description">
-										Lorem ipsum dolor sit amet consectetur adipisicing elit.
-										Fugit obcaecati id neque pariatur veniam atque possimus
-										rerum iusto aperiam error.
-									</p>
-								</section>
-							</article>
-						</div>
-						<div className="testimonials-cards">
-							<article className="testimonial-card">
-								<div className="back-of">
-									<img
-										className="testimonial-image"
-										src="5097224.jpg"
-										alt="prisentation of the person giving the testimonial"
-									/>
-								</div>
+                  <p className="testimonial-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Fugit obcaecati id neque pariatur veniam atque possimus
+                    rerum iusto aperiam error.
+                  </p>
+                </section>
+              </article>
+            </div>
+            <div className="testimonials-cards">
+              <article className="testimonial-card">
+                <div className="back-of">
+                  <img
+                    className="testimonial-image"
+                    src="5097224.jpg"
+                    alt="prisentation of the person giving the testimonial"
+                  />
+                </div>
 
-								<section className="testimonial-info">
-									<header className="testimonial-header">
-										<div className="testimonial-name">
-											<p className="name">John Doe</p>
-										</div>
-										<div className="testimonial-formation">
-											<h2 className="formation">Formation suivie</h2>
-											<p className="level">| Niveau avancé</p>
-										</div>
-									</header>
+                <section className="testimonial-info">
+                  <header className="testimonial-header">
+                    <div className="testimonial-name">
+                      <p className="name">John Doe</p>
+                    </div>
+                    <div className="testimonial-formation">
+                      <h2 className="formation">Formation suivie</h2>
+                      <p className="level">| Niveau avancé</p>
+                    </div>
+                  </header>
 
-									<p className="testimonial-description">
-										Lorem ipsum dolor sit amet consectetur adipisicing elit.
-										Fugit obcaecati id neque pariatur veniam atque possimus
-										rerum iusto aperiam error.
-									</p>
-								</section>
-							</article>
-						</div>
-					</section>
-				</div>
-			</div>
-		</div>
-	);
+                  <p className="testimonial-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Fugit obcaecati id neque pariatur veniam atque possimus
+                    rerum iusto aperiam error.
+                  </p>
+                </section>
+              </article>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -4,19 +4,19 @@ import Testimonials from "./pages/Testimonials/Testimonials";
 import Course from "./styles/Course/Course";
 
 export const router = createBrowserRouter([
-	{
-		path: "/", // The root path
-		element: <App />, // Renders the App component for the home page
-		children: [
-			{
-				path: "/course",
-				element: <Course />,
-			},
-			{
-				path: "/testimonials",
-				element: <Testimonials />,
-			},
-		],
-	},
-	// Try adding a new route! For example, "/about" with an About component
+  {
+    path: "/", // The root path
+    element: <App />, // Renders the App component for the home page
+    children: [
+      {
+        path: "/course",
+        element: <Course />,
+      },
+      {
+        path: "/testimonials",
+        element: <Testimonials />,
+      },
+    ],
+  },
+  // Try adding a new route! For example, "/about" with an About component
 ]);
