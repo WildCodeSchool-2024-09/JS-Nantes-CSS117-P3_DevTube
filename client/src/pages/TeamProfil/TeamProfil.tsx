084470db74905feb1ab2c8667707f801f4f02c8c
ibrahim <iadam606@yahoo.fr>
2025-01-09 17:21:21 +0100

    odif

---
+++ client/src/pages/TeamProfil/TeamProfil.tsx
@@ -0,0 +1,177 @@
+import "/Users/utilisateur/Desktop/projet_3/my-react-app/src/styles.css";
+import "./teamProfil.css";
+const Team_Profil = () => {
+  return (
+    <div>
+      <header className="quation_p">
+        <h1>What’s DevTube ?</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          nihil quam ullam maiores recusandae vero, magni cupiditate facere at
+          dicta quidem, ducimus veritatis vitae illum fugit excepturi sed
+          architecto quas. Magni ipsa asperiores distinctio dolorum quidem
+          eveniet, iusto ea. Fugit sunt dignissimos illo labore iure qui vero
+          tenetur nam tempore, quia aspernatur eum sapiente laborum quaerat
+          nostrum possimus earum facere corrupti ipsa quidem eveniet quam!
+          Tenetur dolores, in minima, est eos aliquid earum recusandae maxime
+          ut, cupiditate iste ipsa beatae?
+        </p>
+      </header>
+      <h2 className="team-job">Our Teame</h2> <br />
+      <div className="cards-sectionf">
+        <section className="cards-section">
+          <img className="img_admine" src="5097224.jpg" alt="img_admine" />
+          <div>
+            <div className="info_head">
+              <div className="info_prsenelle">
+                <h4>Prénom: Ibrahim</h4>
+                <h3>
+                  Nom/ <span>Adam</span>
+                </h3>
+              </div>
+              <div className="icon-admine">
+                <h3>Titre</h3>
+                <div>
+                  <img
+                    className="icon-team"
+                    src="icons8-github-50.png"
+                    alt=""
+                  />
+                  <img className="icon-team" src="icons8-in-50.png" alt="" />
+                </div>
+              </div>
+            </div>
+
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi
+              rerum aliquam sequi earum qui ex.loreme tenetur nam tempore, quia
+              aspernatur eum sapiente laborum quaerat nostrum possimus earum
+            </p>
+          </div>
+        </section>
+        <section className="cards-section">
+          <img className="img_admine" src="5097224.jpg" alt="img_admine" />
+          <div>
+            <div className="info_head">
+              <div className="info_prsenelle">
+                <h4>Prénom: Ibrahim</h4>
+                <h3>
+                  Nom/ <span>Adam</span>
+                </h3>
+              </div>
+              <div className="icon-admine">
+                <h3>Titre</h3>
+                <div>
+                  <img
+                    className="icon-team"
+                    src="icons8-github-50.png"
+                    alt=""
+                  />
+                  <img className="icon-team" src="icons8-in-50.png" alt="" />
+                </div>
+              </div>
+            </div>
+
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi
+              rerum aliquam sequi earum qui ex.loreme tenetur nam tempore, quia
+              aspernatur eum sapiente laborum quaerat nostrum possimus earum
+            </p>
+          </div>
+        </section>
+        <section className="cards-section">
+          <img className="img_admine" src="5097224.jpg" alt="img_admine" />
+          <div>
+            <div className="info_head">
+              <div className="info_prsenelle">
+                <h4>Prénom: Ibrahim</h4>
+                <h3>
+                  Nom/ <span>Adam</span>
+                </h3>
+              </div>
+              <div className="icon-admine">
+                <h3>Titre</h3>
+                <div>
+                  <img
+                    className="icon-team"
+                    src="icons8-github-50.png"
+                    alt=""
+                  />
+                  <img className="icon-team" src="icons8-in-50.png" alt="" />
+                </div>
+              </div>
+            </div>
+
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi
+              rerum aliquam sequi earum qui ex.loreme tenetur nam tempore, quia
+              aspernatur eum sapiente laborum quaerat nostrum possimus earum
+            </p>
+          </div>
+        </section>
+        <section className="cards-section">
+          <img className="img_admine" src="5097224.jpg" alt="img_admine" />
+          <div>
+            <div className="info_head">
+              <div className="info_prsenelle">
+                <h4>Prénom: Ibrahim</h4>
+                <h3>
+                  Nom/ <span>Adam</span>
+                </h3>
+              </div>
+              <div className="icon-admine">
+                <h3>Titre</h3>
+                <div>
+                  <img
+                    className="icon-team"
+                    src="icons8-github-50.png"
+                    alt=""
+                  />
+                  <img className="icon-team" src="icons8-in-50.png" alt="" />
+                </div>
+              </div>
+            </div>
+
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi
+              rerum aliquam sequi earum qui ex.loreme tenetur nam tempore, quia
+              aspernatur eum sapiente laborum quaerat nostrum possimus earum
+            </p>
+          </div>
+        </section>
+        <section className="cards-section">
+          <img className="img_admine" src="5097224.jpg" alt="img_admine" />
+          <div>
+            <div className="info_head">
+              <div className="info_prsenelle">
+                <h4>Prénom: Ibrahim</h4>
+                <h3>
+                  Nom/ <span>Adam</span>
+                </h3>
+              </div>
+              <div className="icon-admine">
+                <h3>Titre</h3>
+                <div>
+                  <img
+                    className="icon-team"
+                    src="icons8-github-50.png"
+                    alt=""
+                  />
+                  <img className="icon-team" src="icons8-in-50.png" alt="" />
+                </div>
+              </div>
+            </div>
+
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi
+              rerum aliquam sequi earum qui ex.loreme tenetur nam tempore, quia
+              aspernatur eum sapiente laborum quaerat nostrum possimus earum
+            </p>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Team_Profil;
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,17 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import TeamProfil from "./pages/TeamProfil/TeamProfil";
 import Course from "./styles/Course/Course";
 
 export const router = createBrowserRouter([
-  {
-    path: "/", // The root path
-    element: <App />, // Renders the App component for the home page
-    children: [
-      {
-        path: "/course",
-        element: <Course />,
-      },
-    ],
-  },
-  // Try adding a new route! For example, "/about" with an About component
+	{
+		path: "/", // The root path
+		element: <App />, // Renders the App component for the home page
+		children: [
+			{
+				path: "/course",
+				element: <Course />,
+			},
+			{
+				path: "/TeamProfil",
+				element: <TeamProfil />,
+			},
+		],
+	},
+	// Try adding a new route! For example, "/about" with an About component
 ]);
