12706bc1b9241110276c583e704b111116256c28
ibrahim <iadam606@yahoo.fr>
2025-01-09 15:57:56 +0100

    Résolvez les conflits avec Git

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,22 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Testimonials from "./components/Testimonials/Testimonials";
+import Testimonials from "./pages/Testimonials/Testimonials";
 import Course from "./styles/Course/Course";
 
 export const router = createBrowserRouter([
-  {
-    path: "/", // The root path
-    element: <App />, // Renders the App component for the home page
-    children: [
-      {
-        path: "/course",
-        element: <Course />,
-      },
-      {
-        path: "/testimonials",
-        element: <Testimonials />,
-      },
-    ],
-  },
-  // Try adding a new route! For example, "/about" with an About component
+	{
+		path: "/", // The root path
+		element: <App />, // Renders the App component for the home page
+		children: [
+			{
+				path: "/course",
+				element: <Course />,
+			},
+			{
+				path: "/testimonials",
+				element: <Testimonials />,
+			},
+		],
+	},
+	// Try adding a new route! For example, "/about" with an About component
 ]);
