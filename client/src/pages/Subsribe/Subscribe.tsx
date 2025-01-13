561236e317075467979ff58243e96d3981e60584
Fabrice Atlan <fabrice.atlan.56@gmail.com>
2025-01-13 17:34:29 +0100

    User subscribe : done.

--- client/src/pages/Subsribe/Subscribe.tsx
+++ client/src/pages/Subsribe/Subscribe.tsx
@@ -1,10 +1,56 @@
+import { useState } from "react";
 import "../../styles/Subscribe.css";
 
 export default function Subscribe() {
+  const [data, setData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    confirm_password: "",
+    github_url: "",
+    linkedin_url: "",
+    profil_img: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3310/api/users", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        // Resetting fields
+        setData({
+          firstname: "",
+          lastname: "",
+          email: "",
+          password: "",
+          confirm_password: "",
+          github_url: "",
+          linkedin_url: "",
+          profil_img: "",
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <section className="main-form-subscribe">
-      <form className="form-subscribe-container">
-        {/* Section firstname */}
+      <form className="form-subscribe-container" onSubmit={handleSubmit}>
         <section className="form-subscribe-label">
           <label id="firstname" htmlFor="firstname">
             First name
@@ -12,13 +58,14 @@ export default function Subscribe() {
           <input
             type="text"
             name="firstname"
+            value={data.firstname}
+            onChange={handleChange}
             aria-labelledby="firstname"
-            placeholder="Enter your firstname."
+            placeholder="Enter your first name."
             required
           />
         </section>
 
-        {/* Section lastname */}
         <section className="form-subscribe-label">
           <label id="lastname" htmlFor="lastname">
             Last name
@@ -26,13 +73,14 @@ export default function Subscribe() {
           <input
             type="text"
             name="lastname"
+            value={data.lastname}
+            onChange={handleChange}
             aria-labelledby="lastname"
-            placeholder="Enter your lastname."
+            placeholder="Enter your last name."
             required
           />
         </section>
 
-        {/* Section email */}
         <section className="form-subscribe-label">
           <label id="email" htmlFor="email">
             Email
@@ -40,13 +88,14 @@ export default function Subscribe() {
           <input
             type="email"
             name="email"
+            value={data.email}
+            onChange={handleChange}
             aria-labelledby="email"
             placeholder="Enter your email."
             required
           />
         </section>
 
-        {/* Section password */}
         <section className="form-subscribe-label">
           <label id="password" htmlFor="password">
             Password
@@ -54,25 +103,72 @@ export default function Subscribe() {
           <input
             type="password"
             name="password"
+            value={data.password}
+            onChange={handleChange}
             aria-labelledby="password"
             placeholder="Enter your password."
             required
           />
         </section>
 
-        {/* Section confirmpassword */}
         <section className="form-subscribe-label">
-          <label id="confirm-password" htmlFor="confirm-password">
+          <label id="confirm_password" htmlFor="confirm_password">
             Confirm your password
           </label>
           <input
             type="password"
-            name="confirm-password"
-            aria-labelledby="confirm-password"
-            placeholder="Enter your password."
+            name="confirm_password"
+            value={data.confirm_password}
+            onChange={handleChange}
+            aria-labelledby="confirm_password"
+            placeholder="Confirm your password."
             required
           />
         </section>
+
+        <section className="form-subscribe-label">
+          <label id="github_url" htmlFor="github_url">
+            GitHub URL
+          </label>
+          <input
+            type="text"
+            name="github_url"
+            value={data.github_url}
+            onChange={handleChange}
+            aria-labelledby="github_url"
+            placeholder="Enter your GitHub."
+          />
+        </section>
+
+        <section className="form-subscribe-label">
+          <label id="linkedin_url" htmlFor="linkedin_url">
+            Linkedin URL
+          </label>
+          <input
+            type="text"
+            name="linkedin_url"
+            value={data.linkedin_url}
+            onChange={handleChange}
+            aria-labelledby="linkedin_url"
+            placeholder="Enter your Linkedin URL."
+          />
+        </section>
+
+        <section className="form-subscribe-label">
+          <label id="profil-image" htmlFor="profil-image">
+            Profil image
+          </label>
+          <input
+            type="image"
+            name="profil_img"
+            value={data.profil_img}
+            onChange={handleChange}
+            aria-labelledby="profil-image"
+            placeholder="Drag the user's profil image."
+            required
+          />
+        </section>
+
         <button type="submit" className="little-cta">
           Sign up
         </button>
--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,18 +1,20 @@
 CREATE TABLE user (
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  email VARCHAR(45) NOT NULL,
-  github_url VARCHAR(50),
-  linkedin_url VARCHAR(50),
   firstname  VARCHAR(45) NOT NULL,
   lastname  VARCHAR(45) NOT NULL,
-  level  INT NOT NULL DEFAULT 0,
-  register_date DATE NOT NULL,
+  email VARCHAR(45) NOT NULL,
+  password VARCHAR(45) NOT NULL,
+  confirm_password VARCHAR(45) NOT NULL,
+  github_url VARCHAR(50),
+  linkedin_url VARCHAR(50),
+  level INT NOT NULL DEFAULT 0,
+  register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   profil_img VARCHAR(200),
-  is_admin  BOOLEAN NOT NULL DEFAULT FALSE
+  is_admin  TINYINT DEFAULT 0 NOT NULL
 );
 
-INSERT INTO user (email, github_url, linkedin_url, firstname, lastname, level, register_date, profil_img, is_admin)
+INSERT INTO user (firstname, lastname, email, password, confirm_password, github_url, linkedin_url, level, register_date, profil_img, is_admin)
 VALUES
-  ("emilie.deduyver", "https://github.com/EmiLy-Ly-san", "https://www.linkedin.com/in/emilie-de-duyver/", "Emilie", "De Duyver", 1, "2025/01/10", "/assets/images/userprofil/profil-emilie.jpg", true);
+  ("Emilie", "De Duyver", "emilie.deduyver", "mdp", "mdp", "https://github.com/EmiLy-Ly-san", "https://www.linkedin.com/in/emilie-de-duyver/", 1, "2025/01/10", "/assets/images/userprofil/profil-emilie.jpg", true);
   
 
