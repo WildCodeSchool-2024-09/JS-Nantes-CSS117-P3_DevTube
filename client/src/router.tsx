061eaa1f572b17e9b6837ade1ca40183760e1fd9
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-01-13 13:55:02 +0100

    path router react for homePage

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -3,12 +3,17 @@ import App from "./App";
 import Course from "./pages/Course/Course";
 import Freemium from "./pages/Freemium/Freemium";
 import Login from "./pages/Login/Login";
+import HomePage from "./pages/homePage/HomePage";
 
 export const router = createBrowserRouter([
   {
     path: "/", // The root path
     element: <App />, // Renders the App component for the home page
     children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
       {
         path: "/login",
         element: <Login />,
