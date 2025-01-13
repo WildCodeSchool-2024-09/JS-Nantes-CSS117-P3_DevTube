2a70519096773c543abc71f263537d6a2552a9ac
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-01-13 13:51:50 +0100

    composant videoCard

--- client/src/components/VideoCard/VideoCard.tsx
+++ client/src/components/VideoCard/VideoCard.tsx
@@ -1,23 +1,47 @@
 import clock from "/clock.png";
 import orangeHeart from "/orange-heart.png";
-import "./../styles/VideoCard.css";
+import "./../../styles/VideoCard.css";
 
-function VideoCard() {
+export interface VideoCardProps {
+  title: string;
+  thumbnailUrl: string;
+  isLarge?: boolean;
+  displayCardInfo?: boolean;
+}
+
+function VideoCard({
+  title,
+  thumbnailUrl,
+  isLarge,
+  displayCardInfo = true,
+}: VideoCardProps) {
   // TODO : variable for time video + favorites gestion with heart empty or orange heart
   return (
     <>
-      <article className="video-card">
-        <section className="card-content">Video</section>
-        <section className="card-footer">
-          <div className="first-row-card-footer">
-            <p>Title video</p>
-            <div className="time-video-wrapper">
-              <img className="clock-icon" src={clock} alt="clock" />
-              <p className="time">60 min</p>
-            </div>
+      <article className={`video-card ${isLarge ? "video-card-large" : ""}`}>
+        <section className="card-content">
+          <div className="video-item">
+            <img
+              src={thumbnailUrl}
+              width={isLarge ? undefined : 300}
+              height={isLarge ? undefined : 168}
+              alt={title}
+              className={`video-thumbnail ${!displayCardInfo ? "with-border-radius" : ""}`}
+            />
           </div>
-          <img className="heart-icon" src={orangeHeart} alt="heart icon" />
         </section>
+        {displayCardInfo ? (
+          <section className="card-footer">
+            <div className="first-row-card-footer">
+              <p>{title}</p>
+              <div className="time-video-wrapper">
+                <img className="clock-icon" src={clock} alt="clock" />
+                <p className="time">60 min</p>
+              </div>
+            </div>
+            <img className="heart-icon" src={orangeHeart} alt="heart icon" />
+          </section>
+        ) : null}
       </article>
     </>
   );
