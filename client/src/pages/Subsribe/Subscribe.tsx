49aa138224184d65d150f69b63c75caf9970ec9c
Fabrice Atlan <fabrice.atlan.56@gmail.com>
2025-01-15 15:31:32 +0100

    Uploading a profile photo to the server is now operational

--- client/src/pages/Subsribe/Subscribe.tsx
+++ client/src/pages/Subsribe/Subscribe.tsx
@@ -1,16 +1,59 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "../../styles/Subscribe.css";
 
 export default function Subscribe() {
   const formRef = useRef<HTMLFormElement | null>(null);
+  const [file, setFile] = useState<File | null>(null);
+  // useState for drag the selected image
+  const [imgSrc, setImageSrc] = useState<string>();
+
+  const handleDragOver = (e: React.DragEvent<HTMLScriptElement>) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLScriptElement>) => {
+    e.preventDefault();
+    const newImage = e.dataTransfer.files[0];
+    if (newImage.type.startsWith("image/")) {
+      const reader = new FileReader();
+      reader.onload = () => setImageSrc(reader.result as string);
+      reader.readAsDataURL(newImage);
+      setFile(newImage);
+    }
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const currentFile = e.target.files ? e.target.files[0] : null;
+    setFile(currentFile);
+  };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const formData = new FormData(e.target as HTMLFormElement);
-    const data = Object.fromEntries(formData.entries());
+    const formDataImage = new FormData();
+
+    if (file) formDataImage.append("profile-image", file);
 
     try {
+      const responseImage = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/users/file`,
+        {
+          method: "POST",
+          body: formDataImage,
+        },
+      );
+
+      if (!responseImage.ok) {
+        throw new Error("Upload error !");
+      }
+
+      const { imageProfileURL } = await responseImage.json();
+
+      const formData = new FormData(e.target as HTMLFormElement);
+      const data = Object.fromEntries(formData.entries());
+
+      data.profil_img = imageProfileURL;
+
       const response = await fetch(
         `${import.meta.env.VITE_API_URL}/api/users`,
         {
@@ -22,6 +65,7 @@ export default function Subscribe() {
 
       if (response.ok) {
         formRef.current?.reset();
+        setFile(null);
       }
     } catch (error) {
       console.error(error);
@@ -29,13 +73,15 @@ export default function Subscribe() {
   };
 
   return (
-    <section className="main-form-subscribe">
-      <form
-        className="form-subscribe-container"
-        ref={formRef}
-        onSubmit={handleSubmit}
-      >
-        <section className="form-subscribe-label">
+    <>
+      <section className="form-subscribe-container">
+        <h1>Subscribe</h1>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          encType="multipart/form-data"
+        >
           <label id="firstname" htmlFor="firstname">
             First name
           </label>
@@ -46,9 +92,7 @@ export default function Subscribe() {
             placeholder="Enter your first name."
             required
           />
-        </section>
 
-        <section className="form-subscribe-label">
           <label id="lastname" htmlFor="lastname">
             Last name
           </label>
@@ -59,9 +103,7 @@ export default function Subscribe() {
             placeholder="Enter your last name."
             required
           />
-        </section>
 
-        <section className="form-subscribe-label">
           <label id="email" htmlFor="email">
             Email
           </label>
@@ -72,9 +114,7 @@ export default function Subscribe() {
             placeholder="Enter your email."
             required
           />
-        </section>
 
-        <section className="form-subscribe-label">
           <label id="password" htmlFor="password">
             Password
           </label>
@@ -85,9 +125,7 @@ export default function Subscribe() {
             placeholder="Enter your password."
             required
           />
-        </section>
 
-        <section className="form-subscribe-label">
           <label id="confirm_password" htmlFor="confirm_password">
             Confirm your password
           </label>
@@ -98,9 +136,7 @@ export default function Subscribe() {
             placeholder="Confirm your password."
             required
           />
-        </section>
 
-        <section className="form-subscribe-label">
           <label id="github_url" htmlFor="github_url">
             GitHub URL
           </label>
@@ -110,9 +146,7 @@ export default function Subscribe() {
             aria-labelledby="github_url"
             placeholder="Enter your GitHub."
           />
-        </section>
 
-        <section className="form-subscribe-label">
           <label id="linkedin_url" htmlFor="linkedin_url">
             Linkedin URL
           </label>
@@ -122,25 +156,30 @@ export default function Subscribe() {
             aria-labelledby="linkedin_url"
             placeholder="Enter your Linkedin URL."
           />
-        </section>
 
-        <section className="form-subscribe-label">
-          <label id="profil-image" htmlFor="profil-image">
-            Profil image
-          </label>
-          <input
-            type="image"
-            name="profil_img"
-            aria-labelledby="profil-image"
-            placeholder="Drag the user's profil image."
-            required
-          />
-        </section>
+          <section
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+            onChange={handleFileChange}
+          >
+            <label htmlFor="profil-image" id="profil-image">
+              Profile image
+            </label>
 
-        <button type="submit" className="little-cta">
-          Sign up
-        </button>
-      </form>
-    </section>
+            <section className="image-container">
+              {imgSrc ? (
+                <img src={imgSrc} alt="User's photo." />
+              ) : (
+                <p>Drag a thumbnail for your video here</p>
+              )}
+            </section>
+          </section>
+
+          <button type="submit" className="little-cta">
+            Sign up
+          </button>
+        </form>
+      </section>
+    </>
   );
 }
