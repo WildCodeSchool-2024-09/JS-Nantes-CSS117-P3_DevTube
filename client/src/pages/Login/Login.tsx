14e7911f91c647e4761887fe793ddcf7435fa60e
Fabrice Atlan <fabrice.atlan.56@gmail.com>
2025-01-15 17:11:36 +0100

    Work in progress.

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -3,34 +3,32 @@ import "../../styles/Login.css";
 export default function Login() {
   return (
     <section className="main-form-login">
-      <form action="submit" method="get" className="form-login-container">
+      {/* <form action="submit" method="get" className="form-login-container"> */}
+      <form>
         {/* Section username */}
-        <section className="form-login-label">
-          <label id="username" htmlFor="username">
-            User name
-          </label>
-          <input
-            type="text"
-            name="username"
-            aria-labelledby="username"
-            placeholder="Enter your username."
-            required
-          />
-        </section>
+        {/* <section className="form-login-label"> */}
+        <label id="username" htmlFor="username">
+          User name
+        </label>
+        <input
+          type="text"
+          name="username"
+          aria-labelledby="username"
+          placeholder="Enter your username."
+          required
+        />
 
         {/* Section password */}
-        <section className="form-login-label">
-          <label id="password" htmlFor="password">
-            Password
-          </label>
-          <input
-            type="text"
-            name="password"
-            aria-labelledby="password"
-            placeholder="Enter your password."
-            required
-          />
-        </section>
+        <label id="password" htmlFor="password">
+          Password
+        </label>
+        <input
+          type="text"
+          name="password"
+          aria-labelledby="password"
+          placeholder="Enter your password."
+          required
+        />
         <button type="button" className="little-cta">
           Sign up
         </button>
