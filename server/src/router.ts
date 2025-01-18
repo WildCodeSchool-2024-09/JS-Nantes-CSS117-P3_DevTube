65c704f66097723356f8bf9cf4b22b33ee63385f
ibrahim <iadam606@yahoo.fr>
2025-01-18 06:39:19 +0100

    modificatino-profile-team

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,39 +1,10 @@
 import path from "node:path";
 import express from "express";
 import type { Request, Response } from "express";
-import multer from "multer";
 
 import userActions from "./modules/user/userActions";
 
 const router = express.Router();
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/assets/images/userprofil/");
-  },
-  filename: (req, file, cb) => {
-    const fileName = path.parse(file.originalname).name;
-    const extension = path.extname(file.originalname);
-    cb(null, `${fileName}${extension}`);
-  },
-});
-
-const upload = multer({ storage });
-
-router.post(
-  "/api/users/file/",
-  upload.single("profile-image"),
-  (req: Request, res: Response) => {
-    if (!req.file) {
-      res.status(400).send({ message: "Erreur : aucun fichier reçu" });
-      return;
-    }
-
-    const imageProfileURL = `assets/images/userprofil/${req.file.filename}`;
-
-    res.status(200).json({ imageProfileURL });
-  },
-);
 
 router.get("/api/users", userActions.browse);
 router.get("/api/users/:id", userActions.read);
