7d56e66f172f2da25accfe3779c763a189feac4a
Fabrice Atlan <fabrice.atlan.56@gmail.com>
2025-01-21 17:07:31 +0100

    Work in progress.

--- client/src/pages/ProfilUser/ProfilUser.tsx
+++ client/src/pages/ProfilUser/ProfilUser.tsx
@@ -8,14 +8,33 @@ export default function ProfilUser() {
       {/* User informations */}
       <fieldset className="fieldset-container">
         <legend>My informations</legend>
-        <p>Emily De Duyver</p>
-        <p>I am student and I wish becoming front-end developper.</p>
-        <p>Beginner</p>
-        <p>emilie.deduyver@gmail.com</p>
-        <p>Freemium member since on 12/12/24</p>
-        <button type="submit" className="standard-button">
-          Update
-        </button>
+
+        <form>
+          <input
+            type="text"
+            name="firstname"
+            aria-labelledby="firstname"
+            placeholder="Emily De Duyver"
+            required
+          />
+
+          <p>I am student and I wish becoming front-end developper.</p>
+          <p>Beginner</p>
+
+          <input
+            type="text"
+            name="firstname"
+            aria-labelledby="firstname"
+            placeholder="emilie.deduyver@gmail.com"
+            required
+          />
+
+          <p>Freemium member since on 12/12/24</p>
+
+          <button type="submit" className="standard-button">
+            Update
+          </button>
+        </form>
       </fieldset>
 
       {/* Progression section */}
@@ -23,7 +42,7 @@ export default function ProfilUser() {
         <h2>My progression</h2>
         <section className="section-progression-bar">
           <p className="progress-bar">Progress bar</p>
-          <button type="button" className="big-cta">
+          <button type="button" className="little-cta">
             Continue
           </button>
         </section>
