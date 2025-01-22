b46579a04056b6aeaebe619fb4cffc77c4d9d785
ibrahim <iadam606@yahoo.fr>
2025-01-22 23:13:29 +0100

    modification sur le badge profile-team

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -4,39 +4,39 @@ import Course from "./pages/Course/Course";
 import Freemium from "./pages/Freemium/Freemium";
 import Login from "./pages/Login/Login";
 import Subscribe from "./pages/Subsribe/Subscribe";
-import HomePage from "./pages/homePage/HomePage";
 import TeamProfile from "./pages/TeamProfile/TeamProfile";
+import HomePage from "./pages/homePage/HomePage";
 
 export const router = createBrowserRouter([
-	{
-		path: "/", // The root path
-		element: <App />, // Renders the App component for the home page
-		children: [
-			{
-				path: "/",
-				element: <HomePage />,
-			},
-			{
-				path: "/subscribe",
-				element: <Subscribe />,
-			},
-			{
-				path: "/teamProfil",
-				element: <TeamProfile />,
-			},
-			{
-				path: "/login",
-				element: <Login />,
-			},
-			{
-				path: "/course",
-				element: <Course />,
-			},
-			{
-				path: "/freemium",
-				element: <Freemium />,
-			},
-		],
-	},
-	// Try adding a new route! For example, "/about" with an About component
+  {
+    path: "/", // The root path
+    element: <App />, // Renders the App component for the home page
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/subscribe",
+        element: <Subscribe />,
+      },
+      {
+        path: "/teamProfil",
+        element: <TeamProfile />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/course",
+        element: <Course />,
+      },
+      {
+        path: "/freemium",
+        element: <Freemium />,
+      },
+    ],
+  },
+  // Try adding a new route! For example, "/about" with an About component
 ]);
