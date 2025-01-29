2a4413931bba9cf8cef5d009662cc314d24111ae
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-01-29 15:45:26 +0100

    authContext types placed  in a type file

--- client/src/contexts/AuhtProvider.tsx
+++ client/src/contexts/AuhtProvider.tsx
@@ -1,12 +1,8 @@
 import { createContext, useState } from "react";
+import type { AuthProps } from "../types/AuthContext";
 import type { Children } from "../types/themeContext";
 
 export const AuthContext = createContext<AuthProps | null>(null);
-
-interface AuthProps {
-  auth: boolean;
-  setAuth: (value: boolean) => void;
-}
 
 export default function AuthProvider({ children }: Children) {
   const [auth, setAuth] = useState(false);
