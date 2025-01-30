8699d8d0bdacd924d6c23a0ba584ee7d873e7d52
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-01-30 09:21:49 +0100

    verification of authent at first page load

--- client/src/contexts/AuhtProvider.tsx
+++ client/src/contexts/AuhtProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import type { AuthProps } from "../types/AuthContext";
 import type { Children } from "../types/themeContext";
 
@@ -6,6 +6,18 @@ export const AuthContext = createContext<AuthProps | null>(null);
 
 export default function AuthProvider({ children }: Children) {
   const [auth, setAuth] = useState(false);
+  useEffect(() => {
+    const currentToken = localStorage.getItem("token");
+    if (currentToken) {
+      fetch(`${import.meta.env.VITE_API_URL}/api/verify-token`, {
+        headers: {
+          Authorization: `Bearer ${currentToken}`,
+        },
+      }).then((response) => {
+        setAuth(response.ok); // return a boolean true or false 200
+      });
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
