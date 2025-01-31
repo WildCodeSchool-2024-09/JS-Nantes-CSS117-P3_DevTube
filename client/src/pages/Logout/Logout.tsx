c873f37cc10a7f89bde615f0f193d6a6a209dfbf
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-01-31 10:35:05 +0100

    correction semantic html

--- client/src/pages/Logout/Logout.tsx
+++ client/src/pages/Logout/Logout.tsx
@@ -2,9 +2,9 @@ import "../../styles/Logout.css";
 
 export default function Logout() {
   return (
-    <div className="logout-page">
+    <section className="logout-page">
       <h1>You have been log out successfully</h1>
       <p>Thanks for your visit 👋 !</p>
-    </div>
+    </section>
   );
 }
