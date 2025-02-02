f8b60ca063283ff140d592473f750fd81cef9239
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-02-02 16:15:10 +0100

    current video info placed in default value of the form

--- client/src/components/FormVideoAdmin/FormVideoAdmin.tsx
+++ client/src/components/FormVideoAdmin/FormVideoAdmin.tsx
@@ -53,7 +53,7 @@ export default function FormVideoAdmin() {
       setVideoToUpdate(videoToFind);
       console.warn({ videoToUpdate });
     }
-
+    setVideosSectionOpen(!isVideosSectionOpen);
     setInfoVideoOpen(!isInfoVideoOpen);
   };
 
@@ -114,86 +114,106 @@ export default function FormVideoAdmin() {
             </button>
           ))}
         </section>
-        {/* <fieldset>
-          <legend>Main information video</legend>
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            // defaultValue={selectedUser?.firstname || ""}
-            required
-          />
-          <label id="publication-date" htmlFor="publication-date">
-            Publication date
-          </label>
-          <input
-            type="text"
-            name="publication-date"
-            // defaultValue={selectedUser?.lastname || ""}
-            required
-          />
-          <label id="duration" htmlFor="duration">
-            Duration
-          </label>
-          <input
-            type="text"
-            name="duration"
-            // defaultValue={selectedUser?.email || ""}
-            required
-          />
-          <label id="description" htmlFor="description">
-            Description
-          </label>
-          <textarea
-            // defaultValue={selectedUser?.level || ""}
-            name="description"
-            maxLength={255}
-            placeholder="Enter your description here..."
-            aria-labelledby="description"
-            required
-          />
-          <label htmlFor="add-hero-slider" className="admin-label-wrapper">
-            Add in hero slider
+        <section className={isInfoVideoOpen ? "" : "hidden"}>
+          <fieldset>
+            <legend>Main information video</legend>
+            <label htmlFor="title">Title</label>
             <input
-              type="checkbox"
-              // checked={selectedUser?.is_admin || false}
-              // onChange={updateUserAdminStatus}
-              id="add-hero-slider"
-              name="add-hero-slider"
-              className="admin-check-box"
+              type="text"
+              id="title"
+              name="title"
+              defaultValue={videoToUpdate?.name}
+              required
             />
-          </label>
-          <label id="category-title" htmlFor="category-title">
-            Title of the catergory
-          </label>
-          <input
-            type="text"
-            name="category-title"
-            readOnly
-            aria-labelledby="category-title"
-            required
-          />
-          <label id="newcategory" htmlFor="newcategory">
-            Select a new catergory
-          </label>{" "}
-          <select name="" id="">
-            <option value="">Front-end</option>
-            <option value="">Back-end</option>
-            <option value="">Bases</option>
-          </select>
-        </fieldset>
-
-        <section
-          // onDragOver={handleDragOver}
-          // onDrop={handleDrop}
-          className="section-img-wrapper"
-        >
+            <label id="publication-date" htmlFor="publication-date">
+              Publication date
+            </label>
+            <input
+              type="text"
+              name="publication-date"
+              defaultValue={videoToUpdate?.added_date}
+              required
+            />
+            <label id="duration" htmlFor="duration">
+              Duration
+            </label>
+            <input
+              type="text"
+              name="duration"
+              defaultValue={videoToUpdate?.duration}
+              required
+            />
+            <label id="description" htmlFor="description">
+              Description
+            </label>
+            <textarea
+              defaultValue={videoToUpdate?.description}
+              name="description"
+              maxLength={255}
+              // placeholder="Enter your description here..."
+              aria-labelledby="description"
+              required
+            />
+            <label
+              htmlFor="add-hero-slider"
+              className="heroSlide-label-wrapper"
+            >
+              Add in hero slider
+              <input
+                type="checkbox"
+                checked={videoToUpdate?.is_heroSlide === 1}
+                // onChange={updateVideoHeroSlidetatus}
+                id="add-hero-slider"
+                name="add-hero-slider"
+                className="admin-check-box"
+              />
+            </label>
+            <label id="category-title" htmlFor="category-title">
+              Title of the catergory
+            </label>
+            <input
+              defaultValue={videoToUpdate?.category_id}
+              type="text"
+              name="category-title"
+              readOnly
+              aria-labelledby="category-title"
+              required
+            />
+            {/* <label id="newcategory" htmlFor="newcategory">
+              Select a new catergory
+            </label>{" "}
+            <select name="" id="">
+              <option value="">Front-end</option>
+              <option value="">Back-end</option>
+              <option value="">Bases</option>
+            </select> */}
+          </fieldset>
           <img
-            src="" //{`${import.meta.env.VITE_API_URL}${selectedUser?.profil_img}`}
-            alt="The thumbnail of the video."
+            src={`${import.meta.env.VITE_API_URL}${videoToUpdate?.preview_image}`}
+            alt="The preview of the video."
           />
+
+          <section>
+            <p>
+              Current file video : <span>{`${videoToUpdate?.thumbnail}`}</span>
+            </p>
+            <input type="file" id="video-file" accept="video/*, .mp4" />
+          </section>
         </section>
+        <button
+          type="button"
+          // onClick={handleUpdateUser}
+          className="btntTtest standard-button"
+        >
+          Update
+        </button>
+        <button
+          type="button"
+          // onClick={handleDeleteUser}
+          className="btntTtest standard-button"
+        >
+          Delete
+        </button>
         {/* 
         <fieldset>
           <label id="subscription-date" htmlFor="subscription-date">
