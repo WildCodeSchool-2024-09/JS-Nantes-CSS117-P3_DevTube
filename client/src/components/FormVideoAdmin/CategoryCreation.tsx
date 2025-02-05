caeaa49d3e23786878d94cf675979395d8fb15d9
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-02-05 12:10:36 +0100

    form + button return integrated and workedin category creation section

--- client/src/components/FormVideoAdmin/CategoryCreation.tsx
+++ client/src/components/FormVideoAdmin/CategoryCreation.tsx
@@ -7,6 +7,17 @@ export default function CategoryCreation({
   isCategoryCreationSectionOpen,
   setIsCategoryCreationSectionOpen,
 }: CategoryCreationProps) {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const data = Object.fromEntries(formData.entries());
+    console.warn(data);
+  };
+
+  const returnClick = () => {
+    setIsCategoryCreationSectionOpen(!isCategoryCreationSectionOpen);
+  };
+
   return (
     <>
       {!isCategoryCreationSectionOpen && (
@@ -19,6 +30,28 @@ export default function CategoryCreation({
         >
           Create a video
         </button>
+      )}
+      {isCategoryCreationSectionOpen && (
+        <section>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="new_category">Title</label>
+            <input
+              type="text"
+              id="new_category"
+              name="new_category"
+              placeholder="Write a new videos category name"
+              required
+            />
+            <button type="submit">Add</button>
+          </form>
+          <button
+            type="button"
+            className="standard-button"
+            onClick={returnClick}
+          >
+            Return
+          </button>
+        </section>
       )}
     </>
   );
