c845c36f61bcf6ea7541e59bc1bb3ca296595330
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-02-06 16:59:17 +0100

    in progress

--- client/src/pages/Admin/UpdateDeleteVideo.tsx
+++ client/src/pages/Admin/UpdateDeleteVideo.tsx
@@ -1,3 +1,15 @@
+// import SearchVideoByCategory from "../../components/FormVideoAdmin/SearchVideoByCategory";
+
 export default function UpdateDeleteVideo() {
-  return <div>UpdateDeleteVideo</div>;
+  return (
+    <>
+      <section className="video-manager-wrapper">
+        <h2 className="title-video-manager">
+          Video manager - Update or Delete a video
+        </h2>
+        {/* <SearchVideoByCategory 
+        /> */}
+      </section>
+    </>
+  );
 }
