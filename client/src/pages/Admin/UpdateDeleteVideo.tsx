b593d92f286470f19958c7366b4297bc93ba5a16
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-02-06 22:01:17 +0100

    update fonctionnality reintegrated + todo fix bug in comment for the moment

--- client/src/pages/Admin/UpdateDeleteVideo.tsx
+++ client/src/pages/Admin/UpdateDeleteVideo.tsx
@@ -9,6 +9,86 @@ export default function UpdateDeleteVideo() {
   const outletContext = useOutletContext<OutletContextVideoManagerProps>();
   const { notifyError, notifySuccess } = useToast();
 
+  const handleUpdateVideo = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target as HTMLFormElement);
+    const data = Object.fromEntries(formData.entries());
+
+    let previewImageToUpload: File | string = "";
+
+    if (data.preview_image instanceof File) {
+      // Nouvelle image sélectionnée
+      previewImageToUpload = data.preview_image;
+    } else if (
+      outletContext.videoToUpdate?.preview_image &&
+      !data.preview_image
+    ) {
+      // On créé un File a partir du chemin de l'image :
+      // (on la charge avec fetch puis on prend le blob qui est retourné pour le transformer en File)
+      const currentPreviewImageFileName =
+        outletContext.videoToUpdate?.preview_image.split("/")[
+          outletContext.videoToUpdate?.preview_image.split("/").length - 1
+        ] as string;
+
+      const currentPreviewImageAsAFile = await fetch(
+        `${import.meta.env.VITE_API_URL}${outletContext.videoToUpdate?.preview_image}`,
+      ).then(async (response) => {
+        const blob = await response.blob();
+
+        const file = new File([blob], currentPreviewImageFileName);
+        return file;
+      });
+      // console.log({ file });
+
+      // Image existante conservée
+      previewImageToUpload = currentPreviewImageAsAFile; // Envoyer le chemin existant
+    }
+    //TODO FIX BUG IMAGE BAD PATH IN THE FILE SYSTEM WHEN IT IS REGISTERED
+
+    // On utilise formData.set pour remplacer la valeur
+    formData.set("preview_image", previewImageToUpload);
+    formData.set("is_heroSlide", data.is_heroSlide ? "1" : "0");
+    formData.set("is_freemium", data.is_freemium ? "1" : "0");
+    formData.set("is_popular", data.is_popular ? "1" : "0");
+    formData.set(
+      "thumbnail",
+      outletContext.videoToUpdate?.thumbnail
+        ? outletContext.videoToUpdate?.thumbnail
+        : data.thumbnail,
+    );
+    // console.log({ data });
+
+    // TODO: Add 'thumbnail' in formData
+    // thumbnail should be data.thumbnail || videoToUpdate?.thumbnail
+    // It should be a File, so i'll probably have to fetch it as for preview8image if videoToUpdate?.thumbnail is a path
+
+    try {
+      // console.log({ data });
+      const token = localStorage.getItem("token");
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/videos/${outletContext.videoToUpdate?.id}`,
+        {
+          method: "PUT",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          body: formData,
+        },
+      );
+      if (!response.ok) {
+        throw new Error("An unknown error occurred.");
+      }
+      notifySuccess(
+        `The video ${outletContext.videoToUpdate?.name} has been updated.`,
+      );
+      outletContext.setVideosSectionOpen(!outletContext.isVideosSectionOpen);
+      outletContext.setInfoVideoOpen(!outletContext.isInfoVideoOpen);
+    } catch (err) {
+      notifyError((err as Error).message);
+    }
+  };
+
   const handleDeleteVideo = async () => {
     try {
       const token = localStorage.getItem("token");
@@ -37,6 +117,11 @@ export default function UpdateDeleteVideo() {
     }
   };
 
+  const closeSection = () => {
+    outletContext.setInfoVideoOpen(!outletContext.isInfoVideoOpen);
+    outletContext.setVideosSectionOpen(!outletContext.isVideosSectionOpen);
+  };
+
   return (
     <>
       <section className="video-manager-wrapper">
@@ -44,7 +129,10 @@ export default function UpdateDeleteVideo() {
           Video manager - Update or Delete a video
         </h2>
         <SearchVideoByCategory />
-        <div className={outletContext.isInfoVideoOpen ? "" : "hidden"}>
+        <form
+          className={outletContext.isInfoVideoOpen ? "" : "hidden"}
+          onSubmit={handleUpdateVideo}
+        >
           <InfoVideoToUpdate videoToUpdate={outletContext.videoToUpdate} />
           <FilesVideo videoToUpdate={outletContext.videoToUpdate} />
           <section className="form-buttons-wrapper">
@@ -58,11 +146,15 @@ export default function UpdateDeleteVideo() {
             >
               Delete
             </button>
-            <NavLink to="/admin/video-manager" className="standard-button">
+            <NavLink
+              to="/admin/video-manager"
+              className="standard-button"
+              onClick={closeSection}
+            >
               Return
             </NavLink>
           </section>
-        </div>
+        </form>
       </section>
     </>
   );
