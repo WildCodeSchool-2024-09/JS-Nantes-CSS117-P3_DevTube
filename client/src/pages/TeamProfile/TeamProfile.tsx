37e3abb447d7a85a77dbc90d3c124350ec5499aa
ibrahim <iadam606@yahoo.fr>
2025-02-14 10:53:09 +0100

    add users

--- client/src/pages/TeamProfile/TeamProfile.tsx
+++ client/src/pages/TeamProfile/TeamProfile.tsx
@@ -1,268 +1,100 @@
+import type { UserAdminI } from "../../types/TeamProfile";
 import useTheme from "../../utils/useTheme";
 import "./../../styles/teamProfile.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function TeamProfile() {
-  const { theme } = useTheme();
-  return (
-    <main>
-      <header className="top-title">
-        <h1>What’s DevTube ?</h1>
-        <p className="top-paragraph">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis placeat
-          assumenda mollitia aliquam laborum, cum dicta, facilis iste blanditiis
-          possimus sapiente sequi? Eos eaque deleniti inventore sit itaque
-          perferendis a pariatur recusandae, temporibus cum, illo cumque soluta
-          nobis odio iusto laudantium fugiat ducimus dolorum rerum aperiam! Illo
-          eligendi ratione fugit distinctio voluptatibus mollitia magnam harum
-          tenetur sunt non aspernatur omnis suscipit, id rerum ipsam deleniti,
-          obcaecati, at sint! Blanditiis velit quam nulla molestias possimus
-          incidunt nihil quod! Voluptates, facilis. Nam!
-        </p>
-      </header>
+	const { theme } = useTheme();
 
-      <div className="all-of-the-cards">
-        <div className="content">
-          <div>
-            <div className="back-of-img">
-              <img className="profile-img" src="profile.jpg" alt="profile" />
-            </div>
-          </div>
+	const [users, setUsers] = useState<UserAdminI[]>([]);
 
-          <section className="team-cards">
-            <div className="team-info">
-              <h3>Prénom</h3>
-              <h3>Nom</h3>
-            </div>
+	useEffect(() => {
+		fetch(`${import.meta.env.VITE_API_URL}/api/teamProfil`)
+			.then((response) => response.json())
+			.then((data) => setUsers(data));
+	}, []);
 
-            <article className="team-profile-icon">
-              <h3>titre</h3>
-              <div>
-                <img
-                  src={theme ? "github-for-light-theme.png" : "github-icon.png"}
-                  alt="github-icon"
-                />
-                <img
-                  src={
-                    theme ? "linkedin-for-light-theme.png" : "linkedin-1121.png"
-                  }
-                  alt="linkedin-icon"
-                />
-              </div>
-            </article>
+	return (
+		<main>
+			<header className="top-title">
+				<h1>What’s DevTube ?</h1>
+				<p className="top-paragraph">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis placeat
+					assumenda mollitia aliquam laborum, cum dicta, facilis iste blanditiis
+					possimus sapiente sequi? Eos eaque deleniti inventore sit itaque
+					perferendis a pariatur recusandae, temporibus cum, illo cumque soluta
+					nobis odio iusto laudantium fugiat ducimus dolorum rerum aperiam! Illo
+					eligendi ratione fugit distinctio voluptatibus mollitia magnam harum
+					tenetur sunt non aspernatur omnis suscipit, id rerum ipsam deleniti,
+					obcaecati, at sint! Blanditiis velit quam nulla molestias possimus
+					incidunt nihil quod! Voluptates, facilis. Nam!
+				</p>
+			</header>
 
-            <div className="profile-content">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Tempora consequatur facilis ducimus, non architecto perferendis,
-                ut, cumque ratione accusamus velit ex ipsum ipsam sit? Cum
-                dolorum doloremque fugit qui unde voluptatem dignissimos autem
-                non
-              </p>
-            </div>
-          </section>
-        </div>
+			<div className="all-of-the-cards">
+				{users.map((el) => (
+					<div className="content" key={el.id}>
+						<div>
+							<div className="back-of-img">
+								<img
+									className="profile-img"
+									src={`${import.meta.env.VITE_API_URL}/${el.profil_img}`}
+									alt="profile"
+								/>
+							</div>
+						</div>
 
-        <div className="content">
-          <div>
-            <div className="back-of-img">
-              <img className="profile-img" src="profile.jpg" alt="profile" />
-            </div>
-          </div>
+						<section className="team-cards">
+							<div className="team-info">
+								<h3>{el.firstname}</h3>
+								<h3>{el.lastname}</h3>
+							</div>
 
-          <section className="team-cards">
-            <div className="team-info">
-              <h3>Prénom</h3>
-              <h3>Nom</h3>
-            </div>
+							<article className="team-profile-icon">
+								<h3>Level...{el.level}</h3>
+								<div>
+									<a
+										href={el.github_url}
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<img
+											src={
+												theme ? "github-for-light-theme.png" : "github-icon.png"
+											}
+											alt="github-icon"
+										/>
+									</a>
+									<a
+										href={el.linkedin_url}
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<img
+											src={
+												theme
+													? "linkedin-for-light-theme.png"
+													: "linkedin-1121.png"
+											}
+											alt="linkedin-icon"
+										/>
+									</a>
+								</div>
+							</article>
 
-            <article className="team-profile-icon">
-              <h3>titre</h3>
-              <div>
-                <img
-                  src={theme ? "github-for-light-theme.png" : "github-icon.png"}
-                  alt="github-icon"
-                />
-                <img
-                  src={
-                    theme ? "linkedin-for-light-theme.png" : "linkedin-1121.png"
-                  }
-                  alt="linkedin-icon"
-                />
-              </div>
-            </article>
-
-            <div className="profile-content">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Tempora consequatur facilis ducimus, non architecto perferendis,
-                ut, cumque ratione accusamus velit ex ipsum ipsam sit? Cum
-                dolorum doloremque fugit qui unde voluptatem dignissimos autem
-                non
-              </p>
-            </div>
-          </section>
-        </div>
-        <div className="content">
-          <div>
-            <div className="back-of-img">
-              <img className="profile-img" src="profile.jpg" alt="profile" />
-            </div>
-          </div>
-
-          <section className="team-cards">
-            <div className="team-info">
-              <h3>Prénom</h3>
-              <h3>Nom</h3>
-            </div>
-
-            <article className="team-profile-icon">
-              <h3>titre</h3>
-              <div>
-                <img
-                  src={theme ? "github-for-light-theme.png" : "github-icon.png"}
-                  alt="github-icon"
-                />
-                <img
-                  src={
-                    theme ? "linkedin-for-light-theme.png" : "linkedin-1121.png"
-                  }
-                  alt="linkedin-icon"
-                />
-              </div>
-            </article>
-
-            <div className="profile-content">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Tempora consequatur facilis ducimus, non architecto perferendis,
-                ut, cumque ratione accusamus velit ex ipsum ipsam sit? Cum
-                dolorum doloremque fugit qui unde voluptatem dignissimos autem
-                non
-              </p>
-            </div>
-          </section>
-        </div>
-        <div className="content">
-          <div>
-            <div className="back-of-img">
-              <img className="profile-img" src="profile.jpg" alt="profile" />
-            </div>
-          </div>
-
-          <section className="team-cards">
-            <div className="team-info">
-              <h3>Prénom</h3>
-              <h3>Nom</h3>
-            </div>
-
-            <article className="team-profile-icon">
-              <h3>titre</h3>
-              <div>
-                <img
-                  src={theme ? "github-for-light-theme.png" : "github-icon.png"}
-                  alt="github-icon"
-                />
-                <img
-                  src={
-                    theme ? "linkedin-for-light-theme.png" : "linkedin-1121.png"
-                  }
-                  alt="linkedin-icon"
-                />
-              </div>
-            </article>
-
-            <div className="profile-content">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Tempora consequatur facilis ducimus, non architecto perferendis,
-                ut, cumque ratione accusamus velit ex ipsum ipsam sit? Cum
-                dolorum doloremque fugit qui unde voluptatem dignissimos autem
-                non
-              </p>
-            </div>
-          </section>
-        </div>
-        <div className="content">
-          <div>
-            <div className="back-of-img">
-              <img className="profile-img" src="profile.jpg" alt="profile" />
-            </div>
-          </div>
-
-          <section className="team-cards">
-            <div className="team-info">
-              <h3>Prénom</h3>
-              <h3>Nom</h3>
-            </div>
-
-            <article className="team-profile-icon">
-              <h3>titre</h3>
-              <div>
-                <img
-                  src={theme ? "github-for-light-theme.png" : "github-icon.png"}
-                  alt="github-icon"
-                />
-                <img
-                  src={
-                    theme ? "linkedin-for-light-theme.png" : "linkedin-1121.png"
-                  }
-                  alt="linkedin-icon"
-                />
-              </div>
-            </article>
-
-            <div className="profile-content">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Tempora consequatur facilis ducimus, non architecto perferendis,
-                ut, cumque ratione accusamus velit ex ipsum ipsam sit? Cum
-                dolorum doloremque fugit qui unde voluptatem dignissimos autem
-                non
-              </p>
-            </div>
-          </section>
-        </div>
-        <div className="content">
-          <div>
-            <div className="back-of-img">
-              <img className="profile-img" src="profile.jpg" alt="profile" />
-            </div>
-          </div>
-
-          <section className="team-cards">
-            <div className="team-info">
-              <h3>Prénom</h3>
-              <h3>Nom</h3>
-            </div>
-
-            <article className="team-profile-icon">
-              <h3>titre</h3>
-              <div>
-                <img
-                  src={theme ? "github-for-light-theme.png" : "github-icon.png"}
-                  alt="github-icon"
-                />
-                <img
-                  src={
-                    theme ? "linkedin-for-light-theme.png" : "linkedin-1121.png"
-                  }
-                  alt="linkedin-icon"
-                />
-              </div>
-            </article>
-
-            <div className="profile-content">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Tempora consequatur facilis ducimus, non architecto perferendis,
-                ut, cumque ratione accusamus velit ex ipsum ipsam sit? Cum
-                dolorum doloremque fugit qui unde voluptatem dignissimos autem
-                non
-              </p>
-            </div>
-          </section>
-        </div>
-      </div>
-    </main>
-  );
+							<div className="profile-content">
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+									Facilis, repellat! Veniam, esse ipsam sit modi consequuntur
+									rem harum doloremque illum mollitia illo, omnis at quod
+									voluptatibus iste officiis totam laborum.
+								</p>
+							</div>
+						</section>
+					</div>
+				))}
+			</div>
+		</main>
+	);
 }
--- server/src/modules/user/userRepository.ts
+++ server/src/modules/user/userRepository.ts
@@ -4,95 +4,95 @@ import type { Result, Rows } from "../../../database/client";
 import type { User } from "./user";
 
 class UserRepository {
-  // Create operation
-  async create(user: User) {
-    // Execute the SQL INSERT query to add a new user to the "user" table
-    const [result] = await databaseClient.query<Result>(
-      "INSERT INTO user (firstname, lastname, email, password, github_url, linkedin_url, profil_img) VALUES (?, ?, ?, ?, ?, ?, ?)",
-      [
-        user.firstname,
-        user.lastname,
-        user.email,
-        user.password,
-        user.github_url,
-        user.linkedin_url,
-        user.profil_img,
-      ],
-    );
-    // Return the ID of the newly inserted user
-    return result.insertId;
-  }
+	// Create operation
+	async create(user: User) {
+		// Execute the SQL INSERT query to add a new user to the "user" table
+		const [result] = await databaseClient.query<Result>(
+			"INSERT INTO user (firstname, lastname, email, password, github_url, linkedin_url, profil_img) VALUES (?, ?, ?, ?, ?, ?, ?)",
+			[
+				user.firstname,
+				user.lastname,
+				user.email,
+				user.password,
+				user.github_url,
+				user.linkedin_url,
+				user.profil_img,
+			],
+		);
+		// Return the ID of the newly inserted user
+		return result.insertId;
+	}
 
-  // Read operations
-  // By id
-  async read(id: number) {
-    // Execute the SQL SELECT query to retrieve a specific item by its ID
-    const [rows] = await databaseClient.query<Rows>(
-      "select * from user where id = ?",
-      [id],
-    );
+	// Read operations
+	// By id
+	async read(id: number) {
+		// Execute the SQL SELECT query to retrieve a specific item by its ID
+		const [rows] = await databaseClient.query<Rows>(
+			"select * from user where id = ?",
+			[id],
+		);
 
-    // Return the first row of the result, which represents the user
-    return rows[0] as User;
-  }
+		// Return the first row of the result, which represents the user
+		return rows[0] as User;
+	}
 
-  //All
-  async readAll() {
-    // Execute the SQL SELECT query to retrieve all items from the "user" table
-    const [rows] = await databaseClient.query<Rows>("select * from user");
+	//All
+	async readAll() {
+		// Execute the SQL SELECT query to retrieve all items from the "user" table
+		const [rows] = await databaseClient.query<Rows>("select * from user");
 
-    // Return the array of items
-    return rows as User[];
-  }
+		// Return the array of items
+		return rows as User[];
+	}
 
-  // Update operation
-  async update(
-    id: string,
-    email: string,
-    github_url: string,
-    linkedin_url: string,
-    firstname: string,
-    lastname: string,
-    level: number,
-    register_date: string,
-    profil_img: string,
-    is_admin: boolean,
-  ) {
-    const [row] = await databaseClient.query<Result>(
-      "UPDATE user SET email = ?, github_url = ?, linkedin_url = ?, firstname = ?, lastname = ?, level = ?, register_date = ?, profil_img = ?, is_admin = ? WHERE id = ?",
-      [
-        email,
-        github_url,
-        linkedin_url,
-        firstname,
-        lastname,
-        level,
-        register_date,
-        profil_img,
-        is_admin,
-        id,
-      ],
-    );
-    return row.affectedRows;
-  }
+	// Update operation
+	async update(
+		id: string,
+		email: string,
+		github_url: string,
+		linkedin_url: string,
+		firstname: string,
+		lastname: string,
+		level: number,
+		register_date: string,
+		profil_img: string,
+		is_admin: boolean,
+	) {
+		const [row] = await databaseClient.query<Result>(
+			"UPDATE user SET email = ?, github_url = ?, linkedin_url = ?, firstname = ?, lastname = ?, level = ?, register_date = ?, profil_img = ?, is_admin = ? WHERE id = ?",
+			[
+				email,
+				github_url,
+				linkedin_url,
+				firstname,
+				lastname,
+				level,
+				register_date,
+				profil_img,
+				is_admin,
+				id,
+			],
+		);
+		return row.affectedRows;
+	}
 
-  // Delete operation
-  async remove(email: string) {
-    const [rows] = await databaseClient.query<Result>(
-      "DELETE FROM user where email = ?",
-      [email],
-    );
-    return rows.affectedRows;
-  }
+	// Delete operation
+	async remove(email: string) {
+		const [rows] = await databaseClient.query<Result>(
+			"DELETE FROM user where email = ?",
+			[email],
+		);
+		return rows.affectedRows;
+	}
 
-  async getUserByEmail(email: string) {
-    const [rows] = await databaseClient.query<Rows>(
-      "SELECT * FROM user WHERE email = ?",
-      [email],
-    );
+	async getUserByEmail(email: string) {
+		const [rows] = await databaseClient.query<Rows>(
+			"SELECT * FROM user WHERE email = ?",
+			[email],
+		);
 
-    return rows[0] as User;
-  }
+		return rows[0] as User;
+	}
 }
 
 export default new UserRepository();
