960982c54f88b0f3e5a04447d37920d3a6f87219
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-02-14 11:31:05 +0100

    swith heart on click

--- client/src/components/VideoCard/VideoCard.tsx
+++ client/src/components/VideoCard/VideoCard.tsx
@@ -1,4 +1,5 @@
 import "./../../styles/VideoCard.css";
+import { useState } from "react";
 import type { VideoCardProps } from "../../types/videocard";
 import useAuth from "../../utils/useAuth";
 import useTheme from "../../utils/useTheme";
@@ -16,6 +17,41 @@ function VideoCard({
 
   const { theme } = useTheme();
   const { auth } = useAuth();
+  const { user } = useAuth();
+  const [isFav, setIsFav] = useState(false);
+
+  const handleClickFav = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    setIsFav(!isFav);
+    console.warn({ event });
+    // const idVideoToFind = event.currentTarget.dataset.idVideo;
+    // try {
+    //   const token = localStorage.getItem("token");
+    //   const response = await fetch(
+    //     `${import.meta.env.VITE_API_URL}/api/videos/${outletContext.videoToUpdate?.id}`,
+    //     {
+    //       method: "DELETE",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //         Authorization: `Bearer ${token}`,
+    //       },
+    //     },
+    //   );
+
+    //   if (!response.ok) {
+    //     throw new Error("An unknown error occurred.");
+    //   }
+    //   notifySuccess(
+    //     `The video ${outletContext.videoToUpdate?.name} has been removed.`,
+    //   );
+    //   outletContext.setVideosSectionOpen(!outletContext.isVideosSectionOpen);
+    //   outletContext.setInfoVideoOpen(!outletContext.isInfoVideoOpen);
+    //   outletContext.setNeedToRefetch(() => !outletContext.needToRefetch);
+    // } catch (err) {
+    //   notifyError((err as Error).message);
+    // }
+  };
 
   return (
     <>
@@ -41,10 +77,16 @@ function VideoCard({
               </div>
             </div>
             {auth && (
-              <button className="fav-button" type="button" data-id={id}>
+              <button
+                className="fav-button"
+                type="button"
+                data-idVideo={id}
+                data-idUser={user?.id}
+                onClick={(event) => handleClickFav(event)}
+              >
                 <img
                   className="heart-icon"
-                  src="/orange-heart.png"
+                  src={isFav ? "/orange-heart.png" : "/empty-heart.png"}
                   alt="heart icon"
                 />
               </button>
