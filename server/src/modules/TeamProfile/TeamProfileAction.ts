feaf26a23ca5e498ece30a60d043ea7b3eb39721
ibrahim <iadam606@yahoo.fr>
2025-02-19 15:16:18 +0100

    fix-profile-team

--- server/src/modules/TeamProfile/TeamProfileAction.ts
+++ server/src/modules/TeamProfile/TeamProfileAction.ts
@@ -2,15 +2,15 @@ import type { RequestHandler } from "express";
 
 import TeamProfileRepository from "./TeamProfileRepository";
 const browse: RequestHandler = async (req, res, next) => {
-	try {
-		const users = await TeamProfileRepository.readAll();
+  try {
+    const users = await TeamProfileRepository.readAll();
 
-		res.json(users);
-	} catch (err) {
-		next(err);
-	}
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
-	browse,
+  browse,
 };
